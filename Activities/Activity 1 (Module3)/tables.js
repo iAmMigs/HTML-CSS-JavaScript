d7e85c6319844e6d9e976d9f5b5f98bcbe8883df
Miguel Osinada <miguelosinada@gmail.com>
2024-01-22 16:53:11 +0800

    Module 3 - Updated activity code

---
+++ Activities/Activity 1 (Module3)/tables.js
@@ -0,0 +1,9 @@
+//Function to create row
+function insertrow(){
+    let table = document.getElementById('mytable');
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+}
